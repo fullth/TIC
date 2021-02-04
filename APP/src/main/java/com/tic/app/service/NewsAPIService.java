0096b56fa21fd06f998d93ada758a6a829a1a82c
User <User@TH>
2021-02-04 14:46:40 +0900

    Add API method.

---
+++ APP/src/main/java/com/tic/app/service/NewsAPIService.java
@@ -0,0 +1,61 @@
+package com.tic.app.service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NewsAPIService {
+
+	@Autowired
+	PropReadService propReadService;
+
+	@SuppressWarnings("rawtypes")
+	public JSONArray getNews(String parse) {
+
+		List getPropArr = propReadService.readProp();
+				
+		JSONArray result = null;
+
+		try {
+			// TODO Http ResponseCode별 처리 추가.
+			String text = URLEncoder.encode(parse, "UTF-8");
+			String apiURL = getPropArr.get(0) + "?query=" + text;
+			
+			URL url = new URL(apiURL);
+
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+			con.setRequestMethod("GET");
+			con.setRequestProperty("X-Naver-Client-Id", (String) getPropArr.get(1));
+			con.setRequestProperty("X-Naver-Client-Secret", (String) getPropArr.get(2));
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = br.readLine()) != null) {
+				response.append(inputLine);
+			}
+
+			JSONObject jsonObj = new JSONObject(response.toString());
+			JSONArray jsonArray = (JSONArray) jsonObj.get("items");
+
+			result = jsonArray;
+			
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return result;
+	}
+}
---
+++ APP/src/main/java/com/tic/app/service/PropReadService.java
@@ -0,0 +1,34 @@
+package com.tic.app.service;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.ibatis.io.Resources;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropReadService {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public List readProp() {
+        
+		String resource = "API.properties";
+        Properties properties = new Properties();
+        
+        List propArr = new ArrayList();
+
+        try {
+            Reader reader = Resources.getResourceAsReader(resource);
+            properties.load(reader);
+            propArr.add(properties.getProperty("REQUEST_URL"));
+            propArr.add(properties.getProperty("CLIENT_ID"));
+            propArr.add(properties.getProperty("CLIENT_SECRET"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+		return propArr;
+	}
+}
