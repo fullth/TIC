e6b2571120571fd6fd8e4858e9f4dd50383ae8ed
User <User@TH>
2021-02-04 14:46:40 +0900

    Add API method.

---
+++ APP/src/main/java/com/tic/app/ParseController.java
@@ -0,0 +1,34 @@
+package com.tic.app;
+
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tic.app.service.NewsAPIService;
+
+@Controller
+public class ParseController {
+
+	@Autowired
+	NewsAPIService newsAPIService;
+	
+	@RequestMapping(value="/getNews")
+	public String newsCrawling(@RequestParam(value="topic")String topic, Model model) throws IOException {
+		
+		JSONArray result = newsAPIService.getNews(topic);
+		
+		for(Object i : result) {
+			//XXX 받아온 JSON데이터의 Description 한글 깨짐. 
+			System.out.println(i);
+			model.addAttribute("result", i);
+		}
+		
+		return "redirect:/";
+	}
+	
+}
--- APP/src/main/webapp/WEB-INF/views/home.jsp
+++ APP/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,3 +1,4 @@
+<%@page import="org.apache.jasper.tagplugins.jstl.core.ForEach"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
@@ -5,10 +6,10 @@
 	<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
+	<form action="/app/getNews">
+		<input type="text" name="topic">
+		<input type="submit" value="submit">
+	</form>
+	${result}
 </body>
 </html>
