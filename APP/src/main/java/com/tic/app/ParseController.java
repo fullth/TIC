5627cec7b41b4667e6042e6cc75e9f3d03085d19
fullth <lth129@naver.com>
2021-02-08 23:12:50 +0900

    Test jstl for print news list.

--- APP/src/main/java/com/tic/app/ParseController.java
+++ APP/src/main/java/com/tic/app/ParseController.java
@@ -1,6 +1,7 @@
 package com.tic.app;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,15 +22,16 @@ public class ParseController {
 	public String newsCrawling(@RequestParam(value="topic")String topic, Model model) throws IOException {
 		
 		List<?> result = newsAPIService.getNews(topic);
+		List<Object> resultList = new ArrayList<Object>();
 		
 		for(Object i : result) {
 			// 받아온 JSON데이터의 Description 한글 깨짐. -> 프로젝트의 인코딩 설정문제.
-			
 			System.out.println(i);
+			resultList.add(i);
 		}
-		model.addAttribute("result", result);
+		model.addAttribute("result", resultList);
 		
 		return "home";
+		// TODO main.jsp에 출력 및 링크 제목으로 기사 본문 요약 가져오기.
 	}
-	
 }
