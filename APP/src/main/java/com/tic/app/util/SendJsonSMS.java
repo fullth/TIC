e1c73aa9347245a6f604039f81616fc58d9a4ba5
fullth <lth129@naver.com>
2021-02-20 23:17:36 +0900

    Test send sms using cool sms api.

---
+++ APP/src/main/java/com/tic/app/util/SendJsonSMS.java
@@ -0,0 +1,50 @@
+package com.tic.app.util;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import java.io.IOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import model.response.GroupModel;
+
+public class SendJsonSMS {
+    public static void main(String[] args) {
+        JsonObject params = new JsonObject();
+        JsonArray messages = new JsonArray();
+
+        JsonObject msg = new JsonObject();
+        msg.addProperty("to", "");
+        msg.addProperty("from", "");
+        msg.addProperty("text", "한글 45자, 영자 90자 이하 입력되면 자동으로 SMS타입의 메시지가 추가됩니다.");
+        messages.add(msg);
+
+        params.add("messages", messages);
+
+        Call<GroupModel> api = APIInit.getAPI().sendMessages(APIInit.getHeaders(), params);
+        api.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<GroupModel> call, Response<GroupModel> response) {
+                // 성공 시 200이 출력됩니다.
+                if (response.isSuccessful()) {
+                    System.out.println("statusCode : " + response.code());
+                    GroupModel body = response.body();
+                    System.out.println("groupId : " + body.getGroupId());
+                    System.out.println("status: " + body.getStatus());
+                    System.out.println("count: " + body.getCount().toString());
+                } else {
+                    try {
+                        System.out.println(response.errorBody().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GroupModel> call, Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
+    }
+}
