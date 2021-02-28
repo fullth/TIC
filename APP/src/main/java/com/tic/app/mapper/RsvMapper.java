1b051e189eda8c4c7e5c97ec22307cb1d79871ba
fullth <lth129@naver.com>
2021-02-28 17:37:14 +0900

    번호등록 로직 테스트

--- APP/src/main/java/com/tic/app/mapper/RsvMapper.java
+++ APP/src/main/java/com/tic/app/mapper/RsvMapper.java
@@ -3,5 +3,8 @@ package com.tic.app.mapper;
 import com.tic.app.model.RsvVO;
 
 public interface RsvMapper {
+	
+	public int selectCountNumber(RsvVO rsvVO) throws Exception;
+	
 	public void insertNumber(RsvVO rsvVO) throws Exception;
 }
