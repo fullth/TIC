620735a23cc685ae232849f2c3a8262fb7f5339a
fullth <47442733+fullth@users.noreply.github.com>
2021-04-05 17:33:20 +0900

    Create ErrBoard.java
    기능추가: ErrorBoard(임시)

---
+++ APP/src/main/java/com/tic/app/model/ErrBoard.java
@@ -0,0 +1,30 @@
+public class ErrBoard {
+	
+	private int idx;
+	private String title;
+	private String contents;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	
+	@Override
+	public String toString() {
+		return "Board [idx=" + idx + ", title=" + title + "]";
+	}
+}
